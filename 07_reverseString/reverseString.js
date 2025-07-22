0486d075294824bb934163e5fe61bccad6df8c61
VinceDeFran <189347931+VinceDeFran@users.noreply.github.com>
2025-07-22 14:37:10 +0700

    Completed the Exercise Follow instructions from README and answered the additional requirements from the spec.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+  // Convert string to array
+  const arr = str.split('');
+
+  // Reverse the array
+  const reversedArr = arr.reverse();
+
+  // Convert array back to string
+  const reversedStr = reversedArr.join('');
+
+  // Return the reversed string
+  return reversedStr;
 };
 
 // Do not edit below this line
