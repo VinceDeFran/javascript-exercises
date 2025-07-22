e30af264ccfb0445ca34873fac4d6a1a1b81c149
VinceDeFran <189347931+VinceDeFran@users.noreply.github.com>
2025-07-22 14:32:45 +0700

    Completed the Exercise Follow instructions from README and answered the additional requirements from the spec.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let newString = '';
+    if (number < 0)
+    {
+        return 'ERROR';
+    }
+    else
+    {
+        for (let i = number; i > 0; i--)
+        {
+            newString = newString + string;
+        }
+    }
+    return newString;
 };
-
+console.log(repeatString('La', 10))
 // Do not edit below this line
 module.exports = repeatString;
