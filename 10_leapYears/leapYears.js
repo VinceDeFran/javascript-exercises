97e772dc951c4cbf58f3396f90f1e47b45ba4c6a
VinceDeFran <189347931+VinceDeFran@users.noreply.github.com>
2025-07-22 14:38:31 +0700

    Completed the Exercise Followed instructions from README and answered the additional requirements from the spec.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,32 @@
-const leapYears = function() {
+//let Year = 700;
+const leapYears = function(number) {
 
+/*
+    function isDiv4(number) 
+    {
+        return number % 4 === 0;
+    }
+
+    function isDiv100(number) 
+    {
+        return number % 100 === 0;
+    }
+    function isDiv400(number) 
+    {
+        return number % 400 === 0;
+    }
+*/
+    if ( (number % 4 === 0) && !( (number % 100 === 0) && !(number % 400 === 0) )  )
+    {
+        return true;
+    } else {
+        return false;
+    }
 };
+//console.log(isDiv4(Year));
+//console.log(isDiv100(Year));
+//console.log(isDiv400(Year));
+//console.log(leapYears(Year));
 
 // Do not edit below this line
 module.exports = leapYears;
