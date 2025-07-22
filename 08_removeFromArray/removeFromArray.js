ece1281cb8e88714b286ad4eea51115901bad205
VinceDeFran <189347931+VinceDeFran@users.noreply.github.com>
2025-07-22 15:08:23 +0700

    Completed the Exercise Followed instructions from README and answered the additional requirements from the spec.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  // Deduplicate and collect unique values to remove (ignore repeated args)
+  const valuesToRemove = new Set(args);
+  // Filter out elements that exactly match any value in valuesToRemove
+  return arr.filter(item => !valuesToRemove.has(item));
 };
 
 // Do not edit below this line
